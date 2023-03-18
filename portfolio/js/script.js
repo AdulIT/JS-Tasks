29ef17d19e8ee3753441eac6b06905f8afe3734c
AdulIT <spilberg.max@bk.ru>
2023-03-18 22:55:48 +0600

    feat: tabs is done

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,3 +1,4 @@
+// Menu Burger
 const hamburger = document.querySelector('.hamburger'),
       closeBtn = document.querySelector('.close'),
       menu =  document.querySelector('.menu')
@@ -13,4 +14,50 @@ closeBtn.addEventListener('click', () =>
 {
     menu.style.display = 'none'
     closeBtn.style.display = 'none'
+})
+
+// Tabs
+const tabsParent = document.querySelector('.tabs-container'),
+      tabs = tabsParent.querySelectorAll('.tabs-item'),
+      tabsContent = document.querySelectorAll('.portfolio__projects')
+
+function hideTabContent()
+{
+    tabsContent.forEach(item =>
+        {
+            item.classList.add('hide')
+            item.classList.remove('show')
+        })
+
+    tabs.forEach(item =>
+        {
+            item.classList.remove('tabs-item__active')
+        })
+}
+
+function showTabContent(i = 0)
+{
+    tabsContent[i].classList.add('show')
+    tabsContent[i].classList.remove('hide')
+    tabs[i].classList.add('tabs-item__active')
+}
+
+hideTabContent()
+showTabContent()
+
+tabsParent.addEventListener('click', (e) =>
+{
+    const target = e.target
+
+    if (target && target.classList.contains('tabs-item'))
+    {
+        tabs.forEach((item, i) =>
+            {
+                if (target == item)
+                {
+                    hideTabContent()
+                    showTabContent(i)
+                }
+            })
+    }
 })
